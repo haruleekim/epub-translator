e9bdc7cff59888d1ec61ab5b47431dc2c78e01d7
Haru Kim <haruleekim@icloud.com>
2025-10-29 10:16:41 +0900

    checkout

--- src/ui/converter.ts
+++ src/ui/converter.ts
@@ -1,37 +1,50 @@
-import * as CSS from "css-select";
 import { render } from "dom-serializer";
-import type { Element, Node } from "domhandler";
+import { type AnyNode, Element } from "domhandler";
 import { parseDocument } from "htmlparser2";
 import Epub from "~/lib/epub";
+import { ContentId } from "~/lib/translator";
 
 const DOM_OPTIONS = { xmlMode: true, decodeEntities: false };
-
 const REFERENCING_ATTRIBUTES = ["src", "href", "xlink:href"];
-const REFERENCING_ELEMENTS_SELECTOR = CSS.compile(
-    REFERENCING_ATTRIBUTES.map((attr) => `*[${attr.replace(":", "\\:")}]`).join(", "),
-);
+export const CONTENT_ID_ATTRIBUTE = "data-translator-content-id";
 
 export async function createContentDocument(epub: Epub, spineIndex: number): Promise<string> {
     const resource = epub.getSpineItem(spineIndex);
     const content = await resource.getBlob().then((blob) => blob.text());
     const doc = parseDocument(content, DOM_OPTIONS);
 
-    for (const element of CSS.selectAll<Node, Element>(
-        REFERENCING_ELEMENTS_SELECTOR,
-        doc,
-        DOM_OPTIONS,
-    )) {
-        for (const attr of REFERENCING_ATTRIBUTES) {
-            const path = element.attribs[attr]
-                ? Epub.resolvePath(element.attribs[attr], resource.path)
-                : null;
-            if (path) {
-                const referencedResource = epub.getResource(path);
-                if (referencedResource) {
-                    element.attribs[attr] = await referencedResource.getBlobUrl();
-                }
+    if (!doc.firstChild) return render(doc, DOM_OPTIONS);
+    let node: AnyNode = doc.firstChild;
+    let contentId = new ContentId([0]);
+
+    while (node.parentNode) {
+        if (contentId.leafOrder() >= node.parentNode.childNodes.length) {
+            contentId = contentId.parent().sibling(1);
+            node = node.parentNode.nextSibling ?? node.parentNode;
+            continue;
+        }
+
+        if (node instanceof Element) {
+            node.attribs[CONTENT_ID_ATTRIBUTE] = contentId.toString();
+
+            for (const attr of REFERENCING_ATTRIBUTES) {
+                const path = node.attribs[attr]
+                    ? Epub.resolvePath(node.attribs[attr], resource.path)
+                    : null;
+                if (!path) continue;
+                const referenced = epub.getResource(path);
+                if (referenced) node.attribs[attr] = await referenced.getBlobUrl();
+            }
+
+            if (node.firstChild) {
+                contentId = contentId.firstChild();
+                node = node.firstChild;
+                continue;
             }
         }
+
+        contentId = contentId.sibling(1);
+        node = node.nextSibling ?? node;
     }
 
     return render(doc, DOM_OPTIONS);
