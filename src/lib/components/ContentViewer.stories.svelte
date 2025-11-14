edb1f655bfec908d17e1a091733935276a737e80
Haru Kim <haruleekim@icloud.com>
2025-11-14 10:40:06 +0900

    checkout

--- src/lib/components/ContentViewer.stories.svelte
+++ src/lib/components/ContentViewer.stories.svelte
@@ -3,16 +3,14 @@
 	import ContentViewer from "$lib/components/ContentViewer.svelte";
 	import Epub from "$lib/core/epub";
 	import sample from "$lib/data/sample.epub?url";
-	import Project from "$lib/project";
 
 	const epub = await Epub.load(sample);
-	const project = Project.create(epub, "eng", "kor");
-	const resource = project.epub.getSpineItem(2)!;
+	const resource = epub.getSpineItem(2)!;
 	const blob = await resource.getBlob();
 
 	async function transformUrl(url: string) {
 		const path = Epub.resolvePath(url, resource.path);
-		const transformedUrl = await project.epub.getResource(path)?.getUrl();
+		const transformedUrl = await epub.getResource(path)?.getUrl();
 		return transformedUrl ?? url;
 	}
 
